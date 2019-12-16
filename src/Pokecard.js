d559ae64a289824df3823ce94395aa5662b9e743
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-12-16 18:25:13 -0500

    test return from pokecard component

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import data from './data';
+
+
+
+function Pokecard() {
+    return (
+        <div className = "poke-card-list">
+            <h1>Poke Card</h1>   
+    
+         </div>
+    );
+}
+
+
+  export default Pokecard;
