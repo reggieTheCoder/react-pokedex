efa03b9acfc2b89a126a9bcc9fa59c5b4754f973
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2019-12-26 12:22:14 -0500

    higher resolution images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,12 +4,14 @@ import './Pokecard.css';
 
   import styled from 'styled-components';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice (-3): number);
 
 function Pokecard(cards) {
-    let imgSrc=`${POKE_API}${cards.id}.png`;
+    let imgSrc=`${POKE_API}${padToThree(cards.id)}.png`;
     return (
         <div className = "Pokecard">
                 <h1 className-Pokecard-title>{cards.name}</h1>
